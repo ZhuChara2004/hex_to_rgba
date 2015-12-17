d2cbc583dc3cde5629e7a2bb8f150174683940bb
Dmitry Zhuk <zhuchara@gmail.com>
2015-12-17 09:09:18 +0200

    class code, readme

--- hex_to_rgba.gemspec
+++ hex_to_rgba.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Dmitry Zhuk"]
   spec.email         = ["zhuchara@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Hex to RGBA}
+  spec.description   = %q{Converts hex to RGBA with opacity}
+  spec.homepage      = "http://example.com"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
--- lib/hex_to_rgba.rb
+++ lib/hex_to_rgba.rb
@@ -1,5 +1,26 @@
 require "hex_to_rgba/version"
 
 module HexToRgba
-  # Your code goes here...
+  class Converter
+    attr_accessor :color, :opacity
+
+    def self.convert color, opacity
+      if (/([a-fA-F]|[0-9]){3,6}/ =~ color).nil?
+        'Wrong color format'
+      else
+        if color.length == 3
+          red = (color[0]*2).to_i 16
+          green = (color[1]*2).to_i 16
+          blue = (color[2]*2).to_i 16
+        elsif color.length == 6
+          red = color[0..1].to_i 16
+          green = color[3..4].to_i 16
+          blue = color[5..6].to_i 16
+        else
+          'Wrong color format'
+        end
+        "rgba(#{red}, #{green}, #{blue}, #{opacity})"
+      end
+    end
+  end
 end
