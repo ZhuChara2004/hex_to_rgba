b0df510cb3095121c5baed1f854baaf678f82410
Dmitry Zhuk <zhuchara@gmail.com>
2015-12-17 11:03:04 +0200

    added check for a # presence

--- lib/hex_to_rgba.rb
+++ lib/hex_to_rgba.rb
@@ -2,20 +2,24 @@ require "hex_to_rgba/version"
 
 module HexToRgba
   class Converter
-    attr_accessor :color, :opacity
-
     def self.convert color, opacity
+      if color.to_s.include? '#'
+        hexadecimal_color = color.delete '#'
+      else
+        hexadecimal_color = color
+      end
+
       if (/([a-fA-F]|[0-9]){3,6}/ =~ color).nil?
         'Wrong color format'
       else
-        if color.length == 3
-          red = (color[0]*2).to_i 16
-          green = (color[1]*2).to_i 16
-          blue = (color[2]*2).to_i 16
-        elsif color.length == 6
-          red = color[0..1].to_i 16
-          green = color[3..4].to_i 16
-          blue = color[5..6].to_i 16
+        if hexadecimal_color.length == 3
+          red = (hexadecimal_color[0]*2).to_i 16
+          green = (hexadecimal_color[1]*2).to_i 16
+          blue = (hexadecimal_color[2]*2).to_i 16
+        elsif hexadecimal_color.length == 6
+          red = hexadecimal_color[0..1].to_i 16
+          green = hexadecimal_color[3..4].to_i 16
+          blue = hexadecimal_color[5..6].to_i 16
         else
           'Wrong color format'
         end
